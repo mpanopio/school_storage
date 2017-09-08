d845b176e3e3b4f4b09861eeb61ef95986fd446e
mpanopio <mpanopio@dons.usfca.edu>
2017-09-07 17:59:22 -0700

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Marius
+ */
+public class ArraySum {
+    int[] array;
+    
+    public void init()
+    {
+        for
+    }
+    
+    public void sum()
+    {
+        int sum = 0;
+        for (int i = 0; i < array.length - 1; i++)
+        {
+            sum = sum + array[i];
+        }
+    }
+    
+    static void main(String[] args)
+    {
+//        int[] array = {1,2,4,8,16,32,64,128,256,512};
+    }
+}
